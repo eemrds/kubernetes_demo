efd816a6999e3527b80b0a9954c8c42fd3c37663
emrds <martinerik@protonmail.com>
2023-09-27 02:48:50 +0200

    fixing pvc

--- simpler_ecom/run.sh
+++ simpler_ecom/run.sh
@@ -1,6 +1,6 @@
 
-kubectl apply -f storageclass.yaml
-kubectl apply -f mongodb-pvc.yaml
+kubectl apply -f local-pv.yaml
+kubectl apply -f local-pvc.yaml
 kubectl apply -f mongodb-deployment.yaml
 kubectl apply -f mongodb-service.yaml
 kubectl apply -f backend-deployment.yaml
